0a10e81763a11780d3cbd9a56c33bfb4ba2dbb57
dallalaste <gabrieldallelaste75@gmail.com>
2025-11-29 15:15:37 -0300

    chore: refazendo a estrutura

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'; 
 import SignupScreen from './pages/credential/signup'; 
 import LoginScreen from './pages/credential/login';
-import TestPage from "./pages/credential/test";
-import CreateProduct from "./pages/product/createProduct";   
-import ViewProducts from './pages/product/viewProducts';
-import CreateCampaignPromotion from './pages/campaignPromotions/createCampaignPromotion';
-import AddProductsToCampaign from './pages/campaignPromotions/addProductsToCampaignPromotion'
 
 export default function App() {
   return (
@@ -15,16 +10,6 @@ export default function App() {
       <Route path="/signup" element={<SignupScreen />} />
 
       <Route path="/login" element={<LoginScreen />} />
-
-      <Route path="/test" element={<TestPage />} />
-
-      <Route path="/createproduct" element={<CreateProduct />} />
-
-      <Route path="/viewproducts" element={<ViewProducts />} />
-
-      <Route path="/createcampaignpromotion" element={<CreateCampaignPromotion />} />
-
-      <Route path="/addproductstocampaign" element={<AddProductsToCampaign />} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
