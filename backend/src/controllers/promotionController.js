95e07c4c5fe6f088b21625ef3f487ef3e8667182
dallalaste <gabrieldallelaste75@gmail.com>
2025-11-29 00:06:38 -0300

    fix: retorno de suas promoções e campanhas

--- backend/src/controllers/promotionController.js
+++ backend/src/controllers/promotionController.js
@@ -7,6 +7,7 @@ async function getPromotionProducts(req, res) {
         if (promotionProducts) {
             return res.status(200).json({message:promotionProducts});
         }
+        return res.status(500).json({message:"Erro ao retornar promoções"});
     } catch (error) {
         return res.status(500).json({message:"Erro crítico"});
     }
@@ -15,7 +16,11 @@ async function getPromotionProducts(req, res) {
 async function getCampaigns(req, res) {
     const {id, userType} = req.user;
     try {
-        
+        const campaigns = await promotionModel.getCampaignsModel(id);
+        if (campaigns) {
+            return res.status(200).json({message:campaigns});
+        }
+        return res.status(500).json({message:"Erro ao retornar campanhas"});
     } catch (error) {
         return res.status(500).json({message:"Erro crítico"});
     }
--- backend/src/models/promotionModel.js
+++ backend/src/models/promotionModel.js
@@ -12,6 +12,19 @@ async function getPromotionProductsModel(id) {
     })
 }
 
+async function getCampaignsModel(id) {
+    return new Promise((resolve, reject) => {
+        const query1 = "SELECT * FROM campaign WHERE salesmanId=?";
+        db.query(query1, [id], (error1, result1) => {
+            if (error1) {
+                return reject(error1);
+            }
+            return resolve(result1);
+        })
+    })
+}
+
 module.exports = {
-    getPromotionProductsModel
+    getPromotionProductsModel,
+    getCampaignsModel
 }
